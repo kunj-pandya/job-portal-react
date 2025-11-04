dd59976524051ee218a8f7ef514871c3c92bc376
kunj-pandya <kunjpandya998@gmail.com>
2025-11-04 11:24:16 +0530

    add Applyout with header component

--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom';
+import Header from '@/components/Header';
 
 const AppLayout = () => {
     return (
         <div>
-            AppLayout
-            <Outlet />
+            <div className='grid-background'></div>
+            <main className='min-h-screen container px-8'>
+                <Header />
+                <Outlet />
+            </main>
+            <div className='p-10 text-center bg-gray-800 mt-10'>Made with ❤ by Kunj Pandya.</div>
         </div>
     )
 }
