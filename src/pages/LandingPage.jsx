08fb9e42d96d61cf8b2c5d2e1172bf6442e527db
kunj-pandya <kunjpandya998@gmail.com>
2025-11-04 19:11:39 +0530

    add LandingPage with titile and with custom style for heading

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -2,7 +2,36 @@ import React from 'react'
 
 const LandingPage = () => {
     return (
-        <div>LandingPage</div>
+        <main className='flex flex-col gap-10 sm:gap-20 py-10 sm:py-20'>
+            <section className='text-center'>
+                <h1 className='flex  flex-col justify-center items-center gradient-title text-4xl font-extrabold sm:text-6xl lg:text-8xl tracking-tighter py-4'>
+                    Find Your Dream Job
+                    <span className='flex items-center gap-2 sm:gap-6'>
+                        and get {""}
+                        <img
+                            src='/logo.png'
+                            alt='Hirred logo'
+                            className='h-14 sm:h-24 lg:h-32'
+
+                        />
+                    </span>
+                </h1>
+
+                <p className='text-gray-300 sm:mt-4 text-xs sm:text-xl'>Explore thousands of job listings or find the perfact candidate</p>
+            </section>
+            <div>
+                {/* buttons */}
+                {/* curousel */}
+            </div>
+
+            {/* banner */}
+
+            <sevtion>
+                {/* cards */}
+            </sevtion>
+
+            {/* Accordion */}
+        </main>
     )
 }
 
